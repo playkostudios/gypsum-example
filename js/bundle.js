6cc1401c9b30fbb581481fe43a472f0d838b0575
rafern <rafern@protonmail.com>
2023-04-06 10:04:42 +0100

    WLE pre-1.0.0 nightlies support (attempt 2, still not ready)

--- js/bundle.js
+++
@@ -1,8 +0,0 @@
-require('@wonderlandengine/components/wasd-controls');
-require('@wonderlandengine/components/mouse-look');
-
-require('./simple-cube-csg');
-require('./extrusion.js');
-require('./manual-memory-management');
-require('./suzanne-coin');
-require('./auto-rotate.js');
